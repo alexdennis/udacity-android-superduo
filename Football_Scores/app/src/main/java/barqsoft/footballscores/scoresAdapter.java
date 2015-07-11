2263cdb866399f5e851f43083464146e9377065d
Alex Dennis <alex.dennis@gmail.com>
2015-07-11 12:41:39 -0400

    Football Scores app has content descriptions for all buttons, images, and text.

--- Football_Scores/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -42,31 +42,54 @@ public class ScoresAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, final Context context, Cursor cursor) {
         final ViewHolder mHolder = (ViewHolder) view.getTag();
-        mHolder.home_name.setText(cursor.getString(COL_HOME));
-        mHolder.away_name.setText(cursor.getString(COL_AWAY));
-        mHolder.date.setText(cursor.getString(COL_MATCHTIME));
-        mHolder.score.setText(Utilies.getScoresString(context, cursor.getInt(COL_HOME_GOALS), cursor.getInt(COL_AWAY_GOALS)));
+
+        String homeTeamName = cursor.getString(COL_HOME);
+        String awayTeamName = cursor.getString(COL_AWAY);
+        String matchTime = cursor.getString(COL_MATCHTIME);
+        String score = Utilies.getScoresString(context, cursor.getInt(COL_HOME_GOALS), cursor.getInt(COL_AWAY_GOALS));
+
+        mHolder.home_name.setText(homeTeamName);
+        mHolder.home_name.setContentDescription(context.getString(R.string.a11y_home_team, homeTeamName));
+
+        mHolder.away_name.setText(awayTeamName);
+        mHolder.away_name.setContentDescription(context.getString(R.string.a11y_away_team, awayTeamName));
+
+        mHolder.date.setText(matchTime);
+        mHolder.date.setContentDescription(context.getString(R.string.a11y_match_time, matchTime));
+
+        mHolder.score.setText(score);
+        mHolder.score.setContentDescription(context.getString(R.string.a11y_score, score));
+
         mHolder.match_id = cursor.getDouble(COL_ID);
+
         mHolder.home_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 context, cursor.getString(COL_HOME)));
         mHolder.away_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 context, cursor.getString(COL_AWAY)));
+
         //Log.v(FetchScoreTask.LOG_TAG,mHolder.home_name.getText() + " Vs. " + mHolder.away_name.getText() +" id " + String.valueOf(mHolder.match_id));
         //Log.v(FetchScoreTask.LOG_TAG,String.valueOf(detail_match_id));
         LayoutInflater vi = (LayoutInflater) context.getApplicationContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = vi.inflate(R.layout.detail_fragment, null);
         ViewGroup container = (ViewGroup) view.findViewById(R.id.details_fragment_container);
-        if (mHolder.match_id == detail_match_id) {
+        if (ifShowDetails(mHolder)) {
             //Log.v(FetchScoreTask.LOG_TAG,"will insert extraView");
 
             container.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT
                     , ViewGroup.LayoutParams.MATCH_PARENT));
+
+            String matchDayDescription = Utilies.getMatchDayString(context, cursor.getInt(COL_MATCHDAY), cursor.getInt(COL_LEAGUE));
+            String leagueDescription = context.getString(Utilies.getLeagueStringResource(cursor.getInt(COL_LEAGUE)));
+
             TextView match_day = (TextView) v.findViewById(R.id.matchday_textview);
-            match_day.setText(Utilies.getMatchDayString(context,
-                    cursor.getInt(COL_MATCHDAY), cursor.getInt(COL_LEAGUE)));
+            match_day.setText(matchDayDescription);
+            match_day.setContentDescription(matchDayDescription);
+
             TextView league = (TextView) v.findViewById(R.id.league_textview);
-            league.setText(context.getString(Utilies.getLeagueStringResource(cursor.getInt(COL_LEAGUE))));
+            league.setText(leagueDescription);
+            league.setContentDescription(context.getString(R.string.a11y_league, leagueDescription));
+
             Button share_button = (Button) v.findViewById(R.id.share_button);
             share_button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -80,6 +103,10 @@ public class ScoresAdapter extends CursorAdapter {
             container.removeAllViews();
         }
 
+    }
+
+    private boolean ifShowDetails(ViewHolder mHolder) {
+        return mHolder.match_id == detail_match_id;
     }
 
     public Intent createShareForecastIntent(String ShareText) {
